ff2b27caf9d3a737ce7e7e32f74ab062c1d67aec
edsonha <edsonha@gmail.com>
2020-05-17 14:08:36 +0800

    [7] Styling Collection-Preview and Collection-Item components

--- src/components/collection-item/collection-item.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-const CollectionItem = ({ name }) => <div>{name}</div>;
-
-export default CollectionItem;
--- src/components/collection-preview/collection-preview.jsx
+++ src/components/collection-preview/collection-preview.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 
-import CollectionItem from "../collection-item/collection-item";
+import CollectionItem from "../collection-item/collection-item.component";
+
+import "./collection-preview.styles.scss";
 
 const CollectionPreview = ({ items, title }) => {
   return (
-    <div>
-      <h1>{title.toUpperCase()}</h1>
-      <div>
+    <div className="collection-preview">
+      <h1 className="title">{title.toUpperCase()}</h1>
+      <div className="preview">
         {items
           .filter((item, index) => index < 4)
           .map(({ id, ...otherItemProps }) => (
