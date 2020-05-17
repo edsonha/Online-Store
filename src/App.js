3bc1baf56389d4f852b08d2edea6db2e757ca6e9
edsonha <edsonha@gmail.com>
2020-05-17 10:45:52 +0800

    [5] Install react-router-dom and use withRouter to prevent props drilling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import React from "react";
 import HomePage from "./pages/homepage/homepage.component";
+import { Switch, Route } from "react-router-dom";
 import "./App.css";
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
+
+const JacketsPage = () => (
+  <div>
+    <h1>JACKETS PAGE</h1>
+  </div>
+);
 
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <HomePage />
+      <div>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/hats" component={HatsPage} />
+          <Route path="/jackets" component={JacketsPage} />
+        </Switch>
       </div>
     );
   }
