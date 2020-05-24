075e512d7ca18f441602cceb42b901130324fb74
edsonha <edsonha@gmail.com>
2020-05-24 20:07:58 +0800

    [33] Implement grouping in Add Item function to keep track of added item quantity

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,4 +1,5 @@
 import { TOGGLE_CART_HIDDEN, ADD_ITEM } from "./cart.types";
+import { addItemToCart } from "./cart.utils";
 
 const intialState = {
   isCartDropdownHidden: true,
@@ -15,7 +16,7 @@ const cartReducer = (state = intialState, action = {}) => {
     case ADD_ITEM:
       return {
         ...state,
-        cartItems: [...state.cartItems, action.payload],
+        cartItems: addItemToCart(state.cartItems, action.payload),
       };
     default:
       return state;
