ede6dcb1d774807891b582bc77856746698ebddd
edsonha <edsonha@gmail.com>
2020-05-24 11:01:00 +0800

    [26] Use ES6 default parameter in the user.reducer

--- src/redux/user/user.reducer.js
+++ src/redux/user/user.reducer.js
@@ -1,10 +1,10 @@
 import { SET_CURRENT_USER } from "./user.types";
 
-const INITIAL_STATE = {
+const intialState = {
   currentUser: null,
 };
 
-const userReducer = (state = INITIAL_STATE, action) => {
+const userReducer = (state = intialState, action = {}) => {
   switch (action.type) {
     case SET_CURRENT_USER:
       return {
