99728e137324a1ac7bc0040aa2c40d9dc801a8e5
edsonha <edsonha@gmail.com>
2020-05-25 22:43:11 +0800

    [47] Implement redux-persist and setting in local storage

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,6 +1,16 @@
 import { combineReducers } from "redux";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage"; //local storage for window browser. Alternatively, there is also session-storage
 
 import userReducer from "./user/user.reducer";
 import cartReducer from "./cart/cart.reducer";
 
-export default combineReducers({ user: userReducer, cart: cartReducer });
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["cart"], //reducer that you want to persist vs blacklist - reducer that you do not want to persist
+};
+
+const rootReducer = combineReducers({ user: userReducer, cart: cartReducer });
+
+export default persistReducer(persistConfig, rootReducer);
