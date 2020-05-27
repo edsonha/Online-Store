ce6d653511eca4ff4a2a49e7e805af42c1153362
edsonha <edsonha@gmail.com>
2020-05-27 23:26:20 +0800

    [51] Use currying method in the shop selector and collection_id_map in order to display the collection based on number id and string url params

---
+++ src/pages/collection/collection.component.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+import { connect } from "react-redux";
+
+import { selectCollection } from "../../redux/shop/shop.selectors";
+
+const CollectionPage = ({ match, collection }) => {
+  console.log("MATCH", match);
+  console.log("COLLECTION", collection);
+  return (
+    <div className="collection-page">
+      <h2>COLLECTION PAGE</h2>
+    </div>
+  );
+};
+
+const mapStateToProps = (state, ownProps) => ({
+  collection: selectCollection(ownProps.match.params.collectionId)(state),
+});
+
+export default connect(mapStateToProps)(CollectionPage);
